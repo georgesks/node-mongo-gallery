0e31e375a6969ae5a0f5c11f5675c7c22af80cf0
jorge gmes <jorge@pilotoautomatico.mx>
2019-02-13 17:55:41 -0600

    Guardando imagen en cloudinary

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,13 @@
 const { Router } = require("express");
+const Photo = require("../models/Photo");
+const cloudinary = require("cloudinary");
+const fs = require("fs-extra");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const router = Router();
 
@@ -14,10 +23,22 @@ router.get("/images/add", (req, res) => {
 
 });
 
-router.post("/images/add", (req, res) => {
-
-    console.log(req.body);
+router.post("/images/add", async (req, res) => {
+    
     console.log(req.file);
+    const { title, description } = req.body;
+    const upload = await cloudinary.v2.uploader.upload(req.file.path);
+    console.log(upload);
+
+    const newPhoto = new Photo({
+        title,
+        description,
+        imageURL: upload.url,
+        public_id: upload.public_id
+    });
+
+    await newPhoto.save();
+    await fs.unlink(req.file.path);
 
     res.send("Recibido");
     
