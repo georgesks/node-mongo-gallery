6c2a96129d85927183af100880249466a64baae7
jorge gmes <jorge@pilotoautomatico.mx>
2019-02-22 14:27:28 -0600

    bug fix: en catch de conexion a mongo

--- src/database.js
+++ src/database.js
@@ -4,4 +4,4 @@ mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true
 })
     .then(db => console.log("MongoDB Conectado !"))
-    .catch(err => console.err("Error al conectar la bd: ", err));
+    .catch(err => console.log("Error al conectar la bd: ", err));
